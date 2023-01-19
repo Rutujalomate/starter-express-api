51b78ef48f80e6152268d7ee3d84054963fb4580
Rutuja Lomate <104217802+Rutujalomate@users.noreply.github.com>
2023-01-19 13:57:57 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
 const main=async()=>{
-    return await mongoose.connect("mongodb://127.0.0.1:27017/masaizon20")
+    return await mongoose.connect("mongodb://127.0.0.1:27017/masaizon20",{ useNewUrlParser: true, useUnifiedTopology: true })
     console.log("conneted")
     conn.disconnect()
 }
